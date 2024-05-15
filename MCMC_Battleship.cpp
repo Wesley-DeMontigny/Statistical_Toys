cdd0c8386075e0dc85f455db80097b968ef3d702
Wesley DeMontigny <168740818+Wesley-DeMontigny@users.noreply.github.com>
2024-05-15 14:27:10 -0400

    Updated Battleship MCMC
    Significant performance improvements. Implemented ships as a class so I don't have to be constantly generating ship positions on the board. Overall, the code is much cleaner.

--- MCMC_Battleship.cpp
+++ MCMC_Battleship.cpp
@@ -19,96 +19,119 @@ swapping between handle and ship positions.
 #include <iterator>
 #include <iomanip>
 
-std::vector<std::vector<bool>> getShipPositions(const std::vector<std::vector<std::tuple<int, unsigned char>>>& board) {
-    int rows = board.size();
-    int cols = board[0].size();
+//Global Game Vars
+const int boardWidth = 10;
+const int boardHeight = 10;
 
-    // Initialize availableSpots
-    std::vector<std::vector<bool>> shipSpots(rows, std::vector<bool>(cols, false));
+//down = 1
+//up = 2
+//right = 3
+//left = 4
 
-    // Mark ship positions
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            int ship = std::get<0>(board[i][j]);
-            if (ship > 0) {
-                // Direction of the ship
-                unsigned char direction = std::get<1>(board[i][j]);
-                for (int k = 0; k < ship; k++) {
-                    int x = i, y = j;
-                    if (direction == 1 && i + k < rows) x = i + k; // Down
-                    else if (direction == 2 && i - k >= 0) x = i - k; // Up
-                    else if (direction == 3 && j + k < cols) y = j + k; // Right
-                    else if (direction == 4 && j - k >= 0) y = j - k; // Left
+struct handle{
+    std::tuple<int, int> pos;
+    int orientation;
+};
 
-                    shipSpots[x][y] = true;
-                }
-            }
-        }
-    }
+class ship{
+    public:
+        int getShipSize(){return shipSize;}
+        handle getHandle(){return myHandle;}
+        std::vector<std::tuple<int, int>> getSpots(){return spots;}
+        std::vector<std::tuple<int, int>> getSurroundingSpots(){return surroundingSpots;}
+        void setActive(bool state){active = state;}
+        bool isActive(){return active;}
 
-    return shipSpots;
-}
+        void changePosition(handle newHandle){
+            std::vector<std::tuple<int, int>> newSpots;
+            std::vector<std::tuple<int, int>> newSurroundingSpots;
 
-std::vector<std::vector<bool>> getOpenPositions(const std::vector<std::vector<std::tuple<int, unsigned char>>>& board) {
-    int rows = board.size();
-    int cols = board[0].size();
+            for (int k = 0; k < shipSize; k++) {
+                int x = std::get<0>(newHandle.pos), y = std::get<1>(newHandle.pos);
+                if (newHandle.orientation == 1 && x + k < boardHeight) x = x + k; // Down
+                else if (newHandle.orientation == 2 && x - k >= 0) x = x - k; // Up
+                else if (newHandle.orientation == 3 && y + k < boardWidth) y = y + k; // Right
+                else if (newHandle.orientation == 4 && y - k >= 0) y = y - k; // Left
 
-    // Initialize availableSpots
-    std::vector<std::vector<bool>> availableSpots(rows, std::vector<bool>(cols, true));
+                newSpots.push_back(std::tuple<int, int>(x, y));
 
-    // Mark ship positions
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            int ship = std::get<0>(board[i][j]);
-            if (ship > 0) {
-                // Direction of the ship
-                unsigned char direction = std::get<1>(board[i][j]);
-                for (int k = 0; k < ship; k++) {
-                    int x = i, y = j;
-                    if (direction == 1 && i + k < rows) x = i + k; // Down
-                    else if (direction == 2 && i - k >= 0) x = i - k; // Up
-                    else if (direction == 3 && j + k < cols) y = j + k; // Right
-                    else if (direction == 4 && j - k >= 0) y = j - k; // Left
-
-                    //Set surrounding values to also be false
-                    for (int dx = -1; dx <= 1; dx++) {
-                        for (int dy = -1; dy <= 1; dy++) {
-                            int nx = x + dx, ny = y + dy;
-                            if (nx >= 0 && nx < rows && ny >= 0 && ny < cols)
-                                availableSpots[nx][ny] = false;
-                        }
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        int nx = x + dx, ny = y + dy;
+                        if (nx >= 0 && nx < boardHeight && ny >= 0 && ny < boardWidth)
+                            newSurroundingSpots.push_back(std::tuple<int, int>(nx, ny));
                     }
                 }
             }
+            spots = newSpots;
+            surroundingSpots = newSurroundingSpots;
+            active = true;
+            myHandle = newHandle;
+        }
+
+        ship(int size) : active(false) {
+            shipSize = size;
+        }
+    private:
+        int shipSize;
+        handle myHandle;
+        bool active;
+        std::vector<std::tuple<int, int>> spots;
+        std::vector<std::tuple<int, int>> surroundingSpots;
+};
+
+std::vector<std::vector<bool>> getBoardMatrix(const std::vector<ship>& ships) {
+    std::vector<std::vector<bool>> result(boardHeight, std::vector<bool>(boardWidth, true));
+
+    for (ship s : ships) {
+        if(s.isActive()){
+            for(std::tuple<int, int> pos : s.getSurroundingSpots()){
+                result[std::get<0>(pos)][std::get<1>(pos)] = false;
+            }
         }
     }
 
-    return availableSpots;
+    return result;
 }
 
-std::set<std::tuple<int, int, unsigned char>> getValidHandles(int ship, const std::vector<std::vector<std::tuple<int, unsigned char>>>& board){
+
+std::vector<std::vector<bool>> getPositionMatrix(const std::vector<ship>& ships) {
+    std::vector<std::vector<bool>> result(boardHeight, std::vector<bool>(boardWidth, false));
+
+    for (ship s : ships) {
+        if(s.isActive()){
+            for(std::tuple<int, int> pos : s.getSpots()){
+                result[std::get<0>(pos)][std::get<1>(pos)] = true;
+            }
+        }
+    }
+
+    return result;
+}
+
+std::vector<handle> getValidHandles(ship myShip, const std::vector<ship>& ships){
     //Get available handle options for a ship, given a board.
-    std::vector<std::vector<bool>> availableSpots = getOpenPositions(board);
-    int rows = availableSpots.size();
-    int cols = availableSpots[0].size();
-    std::set<std::tuple<int, int, unsigned char>> handles;
+    std::vector<std::vector<bool>> availableSpots = getBoardMatrix(ships);
+    std::vector<handle> handles;
+    int shipSize = myShip.getShipSize();
 
     // Go through the availability matrix and detect 
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
+    for(int i = 0; i < boardHeight; i++){
+        for(int j = 0; j < boardWidth; j++){
             int handleDirectionCounts[4] = {0,0,0,0};
             //Iterate through possible positions
-            for(int k = 0; k < ship; k++)
+            for(int k = 0; k < shipSize; k++)
             {
-                if(i+k < rows && availableSpots[i+k][j]) handleDirectionCounts[0]++;
+                if(i+k < boardHeight && availableSpots[i+k][j]) handleDirectionCounts[0]++;
                 if(i-k >= 0 && availableSpots[i-k][j]) handleDirectionCounts[1]++;
-                if(j+k < cols && availableSpots[i][j+k]) handleDirectionCounts[2]++;
+                if(j+k < boardWidth && availableSpots[i][j+k]) handleDirectionCounts[2]++;
                 if(j-k >= 0 && availableSpots[i][j-k]) handleDirectionCounts[3]++;
             }
             //Add good handles
             for(int x = 0; x < 4; x++){
-                if(handleDirectionCounts[x] == ship)
-                    handles.insert(std::tuple<int, int, unsigned char>(i, j, x+1));
+                if(handleDirectionCounts[x] == shipSize){
+                    handles.push_back(handle{{i, j}, x+1});
+                }
             }
         }
     }
@@ -116,47 +139,13 @@ std::set<std::tuple<int, int, unsigned char>> getValidHandles(int ship, const st
     return handles;
 }
 
-void initBoard(const std::vector<int> ships, const int width, const int height, std::mt19937 rng, std::vector<std::vector<std::tuple<int, unsigned char>>>& board){
-    //Generate a starting board
-    for(int i = 0; i < height; i++){
-        std::vector<std::tuple<int, unsigned char>> row;
-        for(int j = 0; j < width; j++){
-            row.push_back(std::tuple<int, unsigned char>(0,0));
-        }
-        board.push_back(row);
-    }
-
-    std::vector<int> myShips(ships);
-    std::shuffle(myShips.begin(), myShips.end(), rng);
-
-    for(int i = 0; i < myShips.size(); i++){
-        std::set<std::tuple<int, int, unsigned char>> handles = getValidHandles(myShips[i], board);
-
-        std::uniform_int_distribution<> distr(0, handles.size() - 1);
-        auto it = handles.begin();
-        std::advance(it, distr(rng));
-
-        board[std::get<0>(*it)][std::get<1>(*it)] = std::tuple<int, unsigned char>(myShips[i], std::get<2>(*it));
-    }
-
+int getProposalIndex(const std::vector<ship>& ships, std::mt19937 rng){
+    std::uniform_int_distribution<> distr(0, ships.size()-1);
+    return distr(rng);
 }
 
-std::tuple<int, int> getProposalLoc(const std::vector<std::vector<std::tuple<int, unsigned char>>>& board, std::mt19937 rng){
-    std::vector<std::tuple<int, int>> shipIndices;
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (std::get<0>(board[i][j]) > 0) {
-                    shipIndices.push_back(std::tuple<int, int>(i,j));
-                }
-        }
-    }
-
-    std::shuffle(shipIndices.begin(), shipIndices.end(), rng);
-    return shipIndices[0];
-}
-
-double getBoardLikelihood(std::vector<std::vector<std::tuple<int, unsigned char>>>& board, std::vector<std::tuple<int,int>> misses){
-    std::vector<std::vector<bool>> shipPoistions = getShipPositions(board);
+double getBoardLikelihood(const std::vector<ship>& ships, std::vector<std::tuple<int,int>> misses){
+    std::vector<std::vector<bool>> shipPoistions = getPositionMatrix(ships);
     for(std::tuple<int,int> miss : misses){
         if(shipPoistions[std::get<0>(miss)][std::get<1>(miss)]) return 0;
     }
@@ -168,25 +157,54 @@ double getBoardLikelihood(std::vector<std::vector<std::tuple<int, unsigned char>
     return 1;
 }
 
-void printBoard(std::vector<std::vector<std::tuple<int, unsigned char>>>& board){
-    std::vector<std::vector<bool>> openPositions = getShipPositions(board);
-    for(int i = 0; i < openPositions.size(); i++){
-        std::cout << "|";
-        for(int j = 0; j < openPositions[0].size(); j++){
+void printBoard(std::vector<ship> ships){
+    std::vector<std::vector<bool>> openPositions = getPositionMatrix(ships);
+    for(int i = 0; i < boardWidth+2; i++)
+        std::cout << "#";
+    std::cout << std::endl;
+
+    for(int i = 0; i < boardHeight; i++){
+        std::cout << "#";
+        for(int j = 0; j < boardWidth; j++){
             if(openPositions[i][j]){
                 std::cout << "X";
             }else{
-                std::cout << " ";
+                std::cout << ".";
             }
         }
-        std::cout << "|" << std::endl;
+        std::cout << "#" << std::endl;
     }
+
+    for(int i = 0; i < boardWidth+2; i++)
+        std::cout << "#";
+    std::cout << std::endl;
+}
+
+void initBoard(std::vector<ship>& ships, std::mt19937 rng){
+    //Generate a starting board
+
+    std::shuffle(ships.begin(), ships.end(), rng);
+    std::cout << "--Initializing Board--" << std::endl;
+    std::cout << "Available Handles:" << std::endl;
+
+    for(ship& s : ships){
+        std::vector<handle> handles = getValidHandles(s, ships);
+        std::cout << "Ship "<< s.getShipSize() << " - " << handles.size() << std::endl;
+
+        std::uniform_int_distribution<> distr(0, handles.size()-1);
+        s.changePosition(handles[distr(rng)]);
+        printBoard(ships);
+    }
+
 }
 
 int main(int argc, const char* argv[]){
-    std::vector<int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
-    int boardWidth = 10;
-    int boardHeight = 10;
+    std::vector<ship> ships;
+    std::vector<int> shipSizes = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+    for(int size : shipSizes){
+        ship newShip(size);
+        ships.push_back(newShip);
+    }
 
     //Observations!
     std::vector<std::tuple<int,int>> misses = {
@@ -201,22 +219,10 @@ int main(int argc, const char* argv[]){
     unsigned seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
     seed ^= std::random_device{}();
     std::mt19937 rng(seed);
-    /*
-    I need my board to easily reflect both the size of the ships, the positions they are taking, and the open positions. 
-    I think the best way to do this is to have the main board consist of handle locations and directions (1-4) and the
-    open locations be calculated from this matrix.
-    This is how the directions of the handles are coded:
-        1 - up
-        2 - down
-        3 - right
-        4 - left
-    */
-    std::vector<std::vector<std::tuple<int, unsigned char>>> board;
 
-    initBoard(ships, boardWidth, boardHeight, rng, board);
-
-    std::cout << "--Initial Board--" << std::endl;
-    printBoard(board);
+    initBoard(ships, rng);
+    std::cout << "\n\n--Initial Board--" << std::endl;
+    printBoard(ships);
 
     std::cout << "\n\n--Starting MCMC--" << std::endl;
     /*
@@ -234,26 +240,24 @@ int main(int argc, const char* argv[]){
     it being inconsistent with the observations. To implement hits, we need to sometimes make proposals according to random changes and
     sometimes try to swap boats around. Additionally, it would also be worth 
     */
-    int chainLength = 1000000;
+    int chainLength = 5000000;
     int burnIn = 1000;
     int sampleRate = 1;
     for(int i = 0; i < chainLength; i++){
-        std::vector<std::vector<std::tuple<int, unsigned char>>> proposal(board);
+        std::vector<ship> proposedShipArrangement(ships);
 
-        std::tuple<int, int> proposalLoc = getProposalLoc(proposal, rng); //Select a ship
-        int adjustedShip = std::get<0>(proposal[std::get<0>(proposalLoc)][std::get<1>(proposalLoc)]);
+        int proposedShip = getProposalIndex(proposedShipArrangement, rng); //Select a ship
+        ship& adjustedShip = proposedShipArrangement.at(proposedShip);
 
-        proposal[std::get<0>(proposalLoc)][std::get<1>(proposalLoc)] = std::tuple<int, unsigned char>(0,0);//Remove the ship from the board
-        std::set<std::tuple<int, int, unsigned char>> handles = getValidHandles(adjustedShip, proposal); //Get potential handles for this ship
+        adjustedShip.setActive(false);//Remove the ship from the board
+        std::vector<handle> handles = getValidHandles(adjustedShip, proposedShipArrangement); //Get potential handles for this ship
         std::uniform_int_distribution<> distr(0, handles.size() - 1);
-        auto it = handles.begin();
-        std::advance(it, distr(rng));
-        proposal[std::get<0>(*it)][std::get<1>(*it)] = std::tuple<int, unsigned char>(adjustedShip, std::get<2>(*it));//Pick a random handle as the proposal
+        adjustedShip.changePosition(handles[distr(rng)]);//Set the handle
 
-        double proposalLikelihood = getBoardLikelihood(proposal, misses);
+        double proposalLikelihood = getBoardLikelihood(proposedShipArrangement, misses);
 
         if(proposalLikelihood == 1.0){
-            board = proposal;
+            ships = proposedShipArrangement;
             //std::cout << "Accepted Proposal: " << proposalLikelihood << std::endl;
         }
         /*else{
@@ -265,7 +269,7 @@ int main(int argc, const char* argv[]){
 
         //Sample past the burn in
         if(i >= burnIn && i % sampleRate == 0){
-            std::vector<std::vector<bool>> shipPoistions = getShipPositions(board);
+            std::vector<std::vector<bool>> shipPoistions = getPositionMatrix(ships);
             for(int j = 0; j < shipPoistions.size(); j++){
                 for(int k = 0; k < shipPoistions[0].size(); k++){
                     if(shipPoistions[j][k]) postBoard[j][k]++;
